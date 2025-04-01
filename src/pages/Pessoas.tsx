82e8ddfbd87307f70aaff2b7b2b8eb79b7b7450e
Diogo Ferreira <diogoveigaferreira@hotmail.com>
2025-03-31 23:15:24 -0300

    feat: tabela com listagem de pessoas

--- src/pages/Pessoas.tsx
+++ src/pages/Pessoas.tsx
@@ -1,42 +1,49 @@
 import { useEffect, useState } from "react";
 import ModelAluno from "../models/ModelAluno";
+import axios from "axios";
 
 const Pessoas = ()=> {
     const [isLoading, setIsLoading] = useState(true);
     const [erro, setErro] = useState(null);
-    const [alunos, setAlunos] = useState<ModelAluno>();
+    const [alunos, setAlunos] = useState<ModelAluno[]>([]);
     useEffect(() => {
         const getData = async ()=>{
-            try{
-                const req = 'http://localhost:3333/pessoas';
-                const response : Response = await fetch(req);
-                
-                if (!response.ok){
-                    throw new Error(`Erro na requisição ${response.status}`);
+        const req = 'http://localhost:3000/pessoas';
+        const response = await axios.get(req)
+            .then(response => {
+                if (!response.status){
+                throw new Error(`Erro na requisição ${response.status}`);
                 }
-                
-                const json = await response.json();
-
-                const converteAlunos = json.map((item: any)=> {
-                    new ModelAluno(item.id, item.nome, item.email, 
+                console.log(response);
+                const converteAlunos = response.data.map((item: any)=> {
+                    return new ModelAluno(item.id, item.nome, item.email, 
                                     item.cpf, item.ativo, item.role, 
                                     item.updatedAt, item.createdAt);
                 });
-            }catch(err){
+                setAlunos(converteAlunos);
+            })
+            .catch(err => {
                 setErro(err.message);
-            }finally{
-                setIsLoading(false);
-            }
-      
+            })
         }
         getData();
+        
+        setIsLoading(false);
     }, []);
 
     if (isLoading)
         return <p>Carregando</p>;
-    if (erro != null)
-        return <p>Erro {erro}</p>;
-
+    if (erro != null){
+        return (
+        <>
+            <div className="toast">
+                <div className="alert alert-info">
+                <span>{`Erro na requisição: ${erro}`}</span>
+                </div>
+            </div>
+        </>
+        )
+    }
     return (
         <>
         <div className="overflow-x-auto">
@@ -52,13 +59,22 @@ const Pessoas = ()=> {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th>1</th>
-                        <td>Cy Ganderton</td>
-                        <td>Quality Control Specialist</td>
-                        <td>Blue</td>
-                    </tr>
+                    {
+                        alunos.map((aluno: ModelAluno) => {
+                        return (
+                            <tr key={aluno.getId()}> {/* Definindo uma chave única para cada linha */}
+                            <td>{aluno.getId()}</td>         {/* Exibindo o ID do aluno */}
+                            <td>{aluno.getNome()}</td>       {/* Exibindo o nome do aluno */}
+                            <td>{aluno.getEmail()}</td>      {/* Exibindo o email do aluno */}
+                            <td>{aluno.getCpf()}</td>        {/* Exibindo o CPF do aluno */}
+                            <td>{aluno.isAtivo() ? 'Sim' : 'Não'}</td> {/* Exibindo se o aluno está ativo ou não */}
+                            <td>{aluno.getRole()}</td>       {/* Exibindo o papel do aluno */}
+                            </tr>
+                        );
+                        })
+                    }
                 </tbody>
+
             </table>
         </div>
         </>
