a3206ce3a6130bf979beef08d63056fe0fc4fa4d
Diogo Ferreira <diogoveigaferreira@hotmail.com>
2025-04-11 11:01:52 -0300

    responsive table and search label

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -42,59 +42,61 @@ const Table = <T,>({data, columns, totalRegistros, totalPaginas, changePaginaPai
     return(
         <div>
             <div className="flex justify-end mb-4">
-                <label className="input w-1/10 transition-all duration-300 hover:w-1/4">
+                <label className="input lg:w-3/10 md:w-2/10 sm:w-1/10 transition-all duration-300 hover:w-4/10">
                     <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2.5" fill="none" stroke="currentColor"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></g></svg>
                     <input type="search" required placeholder="Search" value={search} onChange={onChangeSearch} />
                 </label>
             </div>
-            <table className="table table-zebra table-sm">
-                <thead>
-                    <tr>
-                        {columns.map((column, index)=>(
-                            <th key={index}>{column.header}</th>
-                        ))}                   
-                        <th className="w-3">Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    data.map((item: ModelPessoa, index) => {
-                        return (
-                            <tr key={index}>
-                                {columns.map((column, indexColumn) => (
-                                    <td key={indexColumn}>
-                                                {item[column.acessor]()} 
-                                    </td>
-                                ))}
+            <div className="overflow-x-auto">
+                <table className="table table-zebra table-sm">
+                    <thead>
+                        <tr>
+                            {columns.map((column, index)=>(
+                                <th key={index}>{column.header}</th>
+                            ))}                   
+                            <th className="w-3">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        data.map((item: ModelPessoa, index) => {
+                            return (
+                                <tr key={index}>
+                                    {columns.map((column, indexColumn) => (
+                                        <td key={indexColumn}>
+                                                    {item[column.acessor]()} 
+                                        </td>
+                                    ))}
 
-                                <td className="flex gap-2">
-                                <Modal<ModelPessoa>
-                                    classButton="btn btn-outline p-3 btn-warning"
-                                    idButton={`modal_edit_${item.getId()}`}
-                                    contentButton={<GoPencil />}
-                                    contentModal={columns}
-                                    title="Editar item"
-                                    isEdit = {true}
-                                    model={item}
-                                    onConfirm={(updatedModel)=> handleEdit(updatedModel)}
-                                />
-                                <Modal<ModelPessoa>
-                                    classButton="btn btn-outline p-3 btn-error"
-                                    idButton={`modal_delete_${item.getId()}`}
-                                    contentButton={<MdDelete />}
-                                    contentModal={<p>Deseja excluir <strong>{item.getNome()}</strong>?</p>}
-                                    title="Excluir item"
-                                    model = {item}
-                                    isEdit = {false}
-                                    onConfirm={()=> handleDelete(item.getId())}
-                                />
-                                </td>
-                            </tr>
-                        );
-                    })
-                }
-                </tbody>
-            </table>
+                                    <td className="flex gap-2">
+                                    <Modal<ModelPessoa>
+                                        classButton="btn btn-outline p-3 btn-warning"
+                                        idButton={`modal_edit_${item.getId()}`}
+                                        contentButton={<GoPencil />}
+                                        contentModal={columns}
+                                        title="Editar item"
+                                        isEdit = {true}
+                                        model={item}
+                                        onConfirm={(updatedModel)=> handleEdit(updatedModel)}
+                                    />
+                                    <Modal<ModelPessoa>
+                                        classButton="btn btn-outline p-3 btn-error"
+                                        idButton={`modal_delete_${item.getId()}`}
+                                        contentButton={<MdDelete />}
+                                        contentModal={<p>Deseja excluir <strong>{item.getNome()}</strong>?</p>}
+                                        title="Excluir item"
+                                        model = {item}
+                                        isEdit = {false}
+                                        onConfirm={()=> handleDelete(item.getId())}
+                                    />
+                                    </td>
+                                </tr>
+                            );
+                        })
+                    }
+                    </tbody>
+                </table>
+            </div>
             <div className="flex items-center justify-between mt-4">
                 <div>
                     <span>Total Registros: {Number(totalRegistros)}</span>
